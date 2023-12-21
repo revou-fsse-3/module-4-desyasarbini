487ddf9d64430a21cde87ff384500dd7c820c0aa
desyasarbini <desya.sarbini@gmail.com>
2023-12-22 00:04:13 +0800

    commit perdana

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,6 @@
+const Input = () => {
+    return (
+        <input type="text" />
+    )
+}
+export default Input
---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,6 @@
+const Typography = () => {
+    return (
+        <p></p>
+    )
+}
+export default Typography
